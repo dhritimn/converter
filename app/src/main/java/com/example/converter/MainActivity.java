93755c0b7adad1e519cd45ad209377f12382a48b
dhritimn <minibarsnathsaikia@gmail.com>
2022-04-20 15:49:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/converter/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+private Button button;
+private TextView textView;
+private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Successfully converted", Toast.LENGTH_SHORT).show();
+                String a =  editText.getText().toString();
+                float b = Float.parseFloat(a);
+                float g = 1000*b;
+                textView.setText("The corresponding value in g is: "+g+" g");
+            }
+        });
+    }
+}
